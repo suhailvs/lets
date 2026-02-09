3525e6626c5514cae7658ed3e74fa27c26ad79b9
suhailvs <suhailvs@gmail.com>
2026-02-09 23:52:07 +0530

    fixed Dashboard with pagination

---
+++ frontend/src/pages/Map.jsx
@@ -0,0 +1,17 @@
+import { useParams } from 'react-router';
+
+export default function Map() {
+//   const params = useParams();
+//   const exchangeid = params['id'];
+  return (
+  <div>
+    <div className='row'>
+      <div className='col-12 col-md-4'>
+        <div className="card">
+          MAP
+        </div>
+      </div>
+    </div>
+  </div>
+  )
+}
